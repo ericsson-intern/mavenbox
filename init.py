17f89ae891e8a9825324c7f522743a0ff5d0c46b
Pulkit Garg <wittycodes@gmail.com>
2019-03-19 15:52:36 +0530

    yes

--- init.py
+++ init.py
@@ -1,10 +1,17 @@
 import git, os, shutil
- 
-DIR_NAME = "temp"
+
+
+#============================
+#    os.environment['GIT_ASKPASS']= <full path to your script>
+#    os.environment['GIT_USERNAME'] = <committer username>
+#    os.environment['GIT_PASSWORD'] = <the password>
+#============================
+
+DIR_NAME = "../temp"
 REMOTE_URL = "https://github.com/hasinhayder/LightBulb.git"
  
 if os.path.isdir(DIR_NAME):
-    shutil.rmtree(DIR_NAME)
+    shutil.rmtree(DIR_NAME, ignore_errors=True)
  
 os.mkdir(DIR_NAME)
  
@@ -12,5 +19,5 @@ repo = git.Repo.init(DIR_NAME)
 origin = repo.create_remote('origin',REMOTE_URL)
 origin.fetch()
 origin.pull(origin.refs[0].remote_head)
- 
+git.ex
 print "---- DONE ----"
